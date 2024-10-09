f8bed5b47715767faaa7bff4b9d5277b9bf12a9b
Siddalinga Prasanna H S <siddalingaprasannahs@Siddalingas-MacBook-Pro.local>
2024-10-09 11:42:54 +0530

    Updating naming and reframin files

--- cypress/support/step_definitions/mappings-importer.js
+++ cypress/support/step_definitions/mappings-importer.js
@@ -1,14 +1,14 @@
 import * as loginPage from '../../fixtures/mapping/pageObjects/loginPage';
 import * as drupalPageObjects from '../../fixtures/mapping/pageObjects/durpalPageObjects.json';
 import * as addUserPage from '../../fixtures/mapping/pageObjects/addUserPage.json';
-import * as contactFormSubmission from '../../fixtures/mapping/pageObjects/contactForm.json'
+import * as contactForm from '../../fixtures/mapping/pageObjects/contactForm.json'
 
 
 const selectors = Object.assign(
   loginPage,
   drupalPageObjects,
   addUserPage,
-  contactFormSubmission
+  contactForm
 );
 
 module.exports = selectors;
